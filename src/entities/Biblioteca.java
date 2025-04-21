db65c2cc558b697943750e5c47abaf2b8adf78ac
Andre Urban <andreurbanwork@gmail.com>
2025-04-21 13:54:48 -0300

    Classe Biblioteca metodos

--- src/entities/Biblioteca.java
+++ src/entities/Biblioteca.java
@@ -9,4 +9,33 @@ public class Biblioteca {
 		livros = new ArrayList<>();
 	}
 
+	public void adicionarLivro(Livro livro) {
+		livros.add(livro);
+	}
+
+	public void listarLivros() {
+		if (livros.isEmpty()) {
+			System.out.println("A biblioteca está vazia.");
+		} else {
+			for (int i = 0; i < livros.size(); i++) {
+				System.out.println((i + 1) + ". " + livros.get(i));
+			}
+		}
+	}
+
+	public void emprestarLivro(int indice) {
+		if (indice >= 0 && indice < livros.size()) {
+			livros.get(indice).emprestar();
+		} else {
+			System.out.println("Índice inválido.");
+		}
+	}
+
+	public void devolverLivro(int indice) {
+		if (indice >= 0 && indice < livros.size()) {
+			livros.get(indice).devolver();
+		} else {
+			System.out.println("Índice inválido.");
+		}
+	}
 }
